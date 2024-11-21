015d061387632c0e9583de142099651f1f721f04
riyudev <virtudazoy@gmail.com>
2024-11-22 00:55:04 +0800

    initial

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="bg-slate-200 flex justify-center items-center min-h-screen">
+      <h1 className="font-poppinsRegular">hello world</h1>
+    </div>
+  );
+}
+
+export default App;
