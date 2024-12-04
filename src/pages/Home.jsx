ddbeb536055d2ecad5e765478391bcbea60db36d
quielLovesLasagna <exequielarco23@gmail.com>
2024-12-04 11:00:03 +0800

    eslint configuration, remove unnecessary imports, options missing dependency

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Navbar from "../components/Navbar";
 import Banner from "../assets/movie_banner.jpg";
 import { FaPlay } from "react-icons/fa";
@@ -7,47 +6,47 @@ import MovieCards from "../components/MovieCards";
 import Footer from "../components/Footer.jsx";
 
 function Home() {
-  return (
-    <>
-      <Navbar />
-      <div className="relative">
-        <img src={Banner} alt="" className="h-[110vh] w-full object-cover" />
-        <div className="absolute inset-0 bg-gradient-to-br from-slate-950 to-transparent"></div>
+	return (
+		<>
+			<Navbar />
+			<div className="relative">
+				<img src={Banner} alt="" className="h-[110vh] w-full object-cover" />
+				<div className="absolute inset-0 bg-gradient-to-br from-slate-950 to-transparent"></div>
 
-        <div className="absolute bottom-2 w-full pl-[6%] space-y-6">
-          <figure className="space-y-4 w-[35%]">
-            <h1>The Platform 2</h1>
+				<div className="absolute bottom-2 w-full pl-[6%] space-y-6">
+					<figure className="space-y-4 w-[35%]">
+						<h1>The Platform 2</h1>
 
-            <p className="text-base">
-              After a mysterious leader imposes his law in a brutal system of
-              vertical cells, a new arrival battles against a dubious food
-              distribution method.
-            </p>
+						<p className="text-base">
+							After a mysterious leader imposes his law in a brutal system of
+							vertical cells, a new arrival battles against a dubious food
+							distribution method.
+						</p>
 
-            <div className="flex gap-x-5">
-              <button className="inline-flex justify-center items-center gap-x-3 px-5 py-3 bg-white hover:bg-slate-300 w-[150px]">
-                <FaPlay className="text-lg" />
-                <p className="text-black">Play</p>
-              </button>
-              <button className="flex justify-center items-center gap-x-3 px-5 py-3 bg-slate-500 hover:bg-slate-600 w-[150px]">
-                <GoInfo className="text-white text-xl" />
-                <p>More Info</p>
-              </button>
-            </div>
-          </figure>
+						<div className="flex gap-x-5">
+							<button className="inline-flex justify-center items-center gap-x-3 px-5 py-3 bg-white hover:bg-slate-300 w-[150px]">
+								<FaPlay className="text-lg" />
+								<p className="text-black">Play</p>
+							</button>
+							<button className="flex justify-center items-center gap-x-3 px-5 py-3 bg-slate-500 hover:bg-slate-600 w-[150px]">
+								<GoInfo className="text-xl text-white" />
+								<p>More Info</p>
+							</button>
+						</div>
+					</figure>
 
-          <MovieCards />
-        </div>
-      </div>
-      <div className="ml-[6%]">
-        <MovieCards title={"Blockbuster Movies"} category={"top_rated"} />
-        <MovieCards title={"Only on Netflix"} category={"popular"} />
-        <MovieCards title={"Upcoming"} category={"upcoming"} />
-        <MovieCards title={"Top Picks for You"} category={"now_playing"} />
-      </div>
-      <Footer />
-    </>
-  );
+					<MovieCards />
+				</div>
+			</div>
+			<div className="ml-[6%]">
+				<MovieCards title={"Blockbuster Movies"} category={"top_rated"} />
+				<MovieCards title={"Only on Netflix"} category={"popular"} />
+				<MovieCards title={"Upcoming"} category={"upcoming"} />
+				<MovieCards title={"Top Picks for You"} category={"now_playing"} />
+			</div>
+			<Footer />
+		</>
+	);
 }
 
 export default Home;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,151 +1,151 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import Logo from "../assets/netflix-logo.png";
 import Spinner from "../assets/loading.gif";
 import { MdErrorOutline } from "react-icons/md";
 import { login, signup } from "../Firebase.js";
 
 function Login() {
-  const [signState, setSignState] = useState("Sign In");
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState(false);
-  const [loading, setLoading] = useState(false);
+	const [signState, setSignState] = useState("Sign In");
+	const [name, setName] = useState("");
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [error, setError] = useState(false);
+	const [loading, setLoading] = useState(false);
 
-  const user_auth = async (event) => {
-    event.preventDefault();
-    setLoading(true);
-    setError(false);
-    try {
-      if (signState === "Sign In") {
-        await login(email, password);
-      } else {
-        await signup(name, email, password);
-      }
-    } catch (err) {
-      setError(true);
-    }
-    setLoading(false);
-  };
+	const user_auth = async (event) => {
+		event.preventDefault();
+		setLoading(true);
+		setError(false);
+		try {
+			if (signState === "Sign In") {
+				await login(email, password);
+			} else {
+				await signup(name, email, password);
+			}
+		} catch (err) {
+			setError(true);
+		}
+		setLoading(false);
+	};
 
-  const toggleSignState = () => {
-    setLoading(true);
-    setTimeout(() => {
-      setSignState((prevState) =>
-        prevState === "Sign In" ? "Sign Up" : "Sign In"
-      );
-      setName("");
-      setEmail("");
-      setPassword("");
-      setError(false);
-      setLoading(false);
-    }, 1000);
-  };
+	const toggleSignState = () => {
+		setLoading(true);
+		setTimeout(() => {
+			setSignState((prevState) =>
+				prevState === "Sign In" ? "Sign Up" : "Sign In"
+			);
+			setName("");
+			setEmail("");
+			setPassword("");
+			setError(false);
+			setLoading(false);
+		}, 1000);
+	};
 
-  return loading ? (
-    <div className="flex justify-center items-center h-screen">
-      <img src={Spinner} alt="" className="w-32" />
-    </div>
-  ) : (
-    <div
-      className="flex flex-col items-center justify-center h-screen bg-cover bg-center"
-      style={{ backgroundImage: `url(/bglogin.jpg)` }}
-    >
-      <img src={Logo} alt="" className="absolute top-[3%] left-[6%] w-36" />
+	return loading ? (
+		<div className="flex items-center justify-center h-screen">
+			<img src={Spinner} alt="" className="w-32" />
+		</div>
+	) : (
+		<div
+			className="flex flex-col items-center justify-center h-screen bg-center bg-cover"
+			style={{ backgroundImage: `url(/bglogin.jpg)` }}
+		>
+			<img src={Logo} alt="" className="absolute top-[3%] left-[6%] w-36" />
 
-      <form
-        className="flex flex-col bg-black bg-opacity-60 p-10 px-12 space-y-8 rounded-md max-w-[400px] w-full"
-        onSubmit={user_auth}
-      >
-        <h2 className="font-poppinsBold tracking-wider">{signState}</h2>
+			<form
+				className="flex flex-col bg-black bg-opacity-60 p-10 px-12 space-y-8 rounded-md max-w-[400px] w-full"
+				onSubmit={user_auth}
+			>
+				<h2 className="tracking-wider font-poppinsBold">{signState}</h2>
 
-        <div className="flex flex-col space-y-4 font-poppinsRegular">
-          <div
-            className={`flex space-x-1 justify-center items-center ${
-              error ? "opacity-100" : "opacity-0"
-            }`}
-          >
-            <MdErrorOutline className="text-red-600 text-sm" />
-            <p className="text-red-600 text-xs text-center">
-              Invalid credentials!
-            </p>
-          </div>
+				<div className="flex flex-col space-y-4 font-poppinsRegular">
+					<div
+						className={`flex space-x-1 justify-center items-center ${
+							error ? "opacity-100" : "opacity-0"
+						}`}
+					>
+						<MdErrorOutline className="text-sm text-red-600" />
+						<p className="text-xs text-center text-red-600">
+							Invalid credentials!
+						</p>
+					</div>
 
-          {signState === "Sign Up" && (
-            <input
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              type="text"
-              placeholder="Your Name"
-              required
-              className="rounded-sm"
-            />
-          )}
+					{signState === "Sign Up" && (
+						<input
+							value={name}
+							onChange={(e) => setName(e.target.value)}
+							type="text"
+							placeholder="Your Name"
+							required
+							className="rounded-sm"
+						/>
+					)}
 
-          <input
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            type="email"
-            placeholder="Email"
-            required
-            className="rounded-sm"
-          />
-          <input
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            type="password"
-            placeholder="Password"
-            required
-            className="rounded-sm"
-          />
-        </div>
+					<input
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+						type="email"
+						placeholder="Email"
+						required
+						className="rounded-sm"
+					/>
+					<input
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+						type="password"
+						placeholder="Password"
+						required
+						className="rounded-sm"
+					/>
+				</div>
 
-        <button
-          type="submit"
-          className="bg-red-600 hover:bg-red-700 rounded-sm py-3"
-        >
-          <p>{signState}</p>
-        </button>
+				<button
+					type="submit"
+					className="py-3 bg-red-600 rounded-sm hover:bg-red-700"
+				>
+					<p>{signState}</p>
+				</button>
 
-        <div className="flex justify-between">
-          <div className="flex items-center space-x-1">
-            <input
-              type="checkbox"
-              className="size-4 focus:ring-transparent checked:text-black checked:border-none cursor-pointer"
-            />
-            <label className="text-sm" htmlFor="">
-              Remember Me
-            </label>
-          </div>
-          <p className="text-sm cursor-pointer">Need Help?</p>
-        </div>
+				<div className="flex justify-between">
+					<div className="flex items-center space-x-1">
+						<input
+							type="checkbox"
+							className="cursor-pointer size-4 focus:ring-transparent checked:text-black checked:border-none"
+						/>
+						<label className="text-sm" htmlFor="">
+							Remember Me
+						</label>
+					</div>
+					<p className="text-sm cursor-pointer">Need Help?</p>
+				</div>
 
-        <div className="text-sm">
-          {signState === "Sign In" ? (
-            <p>
-              New to Netflix?{" "}
-              <span
-                onClick={toggleSignState}
-                className="underline text-blue-600 cursor-pointer"
-              >
-                Sign Up Now
-              </span>
-            </p>
-          ) : (
-            <p>
-              Already have an Account?{" "}
-              <span
-                onClick={toggleSignState}
-                className="underline text-blue-600 cursor-pointer"
-              >
-                Sign In Now
-              </span>
-            </p>
-          )}
-        </div>
-      </form>
-    </div>
-  );
+				<div className="text-sm">
+					{signState === "Sign In" ? (
+						<p>
+							New to Netflix?{" "}
+							<span
+								onClick={toggleSignState}
+								className="text-blue-600 underline cursor-pointer"
+							>
+								Sign Up Now
+							</span>
+						</p>
+					) : (
+						<p>
+							Already have an Account?{" "}
+							<span
+								onClick={toggleSignState}
+								className="text-blue-600 underline cursor-pointer"
+							>
+								Sign In Now
+							</span>
+						</p>
+					)}
+				</div>
+			</form>
+		</div>
+	);
 }
 
 export default Login;
