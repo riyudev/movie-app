2fb2637fcae51dee21b331166ce67971e1d8837e
riyudev <virtudazoy@gmail.com>
2024-12-05 21:55:20 +0800

    try fix error

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,6 @@
-const flowbite = require("flowbite-react/tailwind");
-
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", flowbite.content()],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     screens: {
       laptop: "900px",
@@ -18,5 +16,5 @@ export default {
       },
     },
   },
-  plugins: [require("tailwind-scrollbar"), flowbite.plugin()],
+  plugins: [require("tailwind-scrollbar")],
 };
